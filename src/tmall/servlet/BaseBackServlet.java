89796ada3b48d568ce9b8cba9007962ff76c159b
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-11 22:58:17 +0800

    commit src/ file

---
+++ src/tmall/servlet/BaseBackServlet.java
@@ -0,0 +1,18 @@
+package tmall.servlet;
+
+import tmall.dao.CategoryDAO;
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class BaseBackServlet extends HttpServlet {
+    public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String delete(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String edit(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String update(HttpServletRequest request, HttpServletResponse response, Page page);
+    public abstract String list(HttpServletRequest request, HttpServletResponse response, Page page);
+
+    protected CategoryDAO categoryDAO= new CategoryDAO();
+}
