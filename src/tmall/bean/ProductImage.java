c8af555e29ef74b1c3e1d68fbe047064c4e13700
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-13 16:53:45 +0800

    commit  src/tmall/bean/ProductImage.java file

--- src/tmall/bean/ProductImage.java
+++ src/tmall/bean/ProductImage.java
@@ -1,4 +1,34 @@
 package tmall.bean;
 
 public class ProductImage {
+    private String type;
+    private int id;
+    private Product product;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+
 }
