d3a08f6b9cc8a20b9ebb15ca82eff6fa2938c207
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-01-18 17:03:53 +0800

    commit src/tmall/bean/Order.java

--- src/tmall/bean/Order.java
+++ src/tmall/bean/Order.java
@@ -1,4 +1,17 @@
 package tmall.bean;
 
+import java.util.List;
+
 public class Order {
+    public int getId() {
+    }
+
+    public void setTotal(float total) {
+    }
+
+    public void setOrderItems(List<OrderItem> ois) {
+    }
+
+    public void setTotalNumber(int totalNumber) {
+    }
 }
