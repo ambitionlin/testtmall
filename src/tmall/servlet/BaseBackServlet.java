cdfaa8892ba437c1bebc0cab4bc746a1987a1ff6
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-02-06 22:23:15 +0800

    src/tmall/servlet/BaseBackServlet.java

--- src/tmall/servlet/BaseBackServlet.java
+++ src/tmall/servlet/BaseBackServlet.java
@@ -1,14 +1,28 @@
 package tmall.servlet;
 
-import tmall.dao.CategoryDAO;
-import tmall.dao.ProductImageDAO;
-import tmall.dao.PropertyDAO;
-import tmall.dao.UserDAO;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import tmall.bean.OrderItem;
+import tmall.bean.PropertyValue;
+import tmall.bean.Review;
+import tmall.dao.*;
 import tmall.util.Page;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public abstract class BaseBackServlet extends HttpServlet {
     public abstract String add(HttpServletRequest request, HttpServletResponse response, Page page);
@@ -21,6 +35,83 @@ public abstract class BaseBackServlet extends HttpServlet {
     protected UserDAO userDAO = new UserDAO();
     protected PropertyDAO propertyDAO = new PropertyDAO();
     protected ProductImageDAO productImageDAO = new ProductImageDAO();
+    protected OrderDAO orderDAO = new OrderDAO();
+    protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+    protected ProductDAO productDAO = new ProductDAO();
+    protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+    protected ReviewDAO reviewDAO = new ReviewDAO();
 
+    public void service(HttpServletRequest request,HttpServletResponse response){
+        try{
+            /*获取分页信息*/
+            int start = 0;
+            int count = 5;
+            try {
+                start = Integer.parseInt(request.getParameter("page.start"));
+            }catch (Exception e){
 
+            }
+
+            try{
+                count = Integer.parseInt(request.getParameter("page.count"));
+            }catch (Exception e){
+
+            }
+            Page page = new Page(start,count);
+
+            /*借助反射，调用对应的方法*/
+            String method = (String) request.getAttribute("method");
+            Method m = this.getClass().getMethod(method,javax.servlet.http.HttpServletRequest.class,
+                    javax.servlet.http.HttpServletResponse.class,Page.class);
+            String redirect = m.invoke(this,request,response,page).toString();
+
+            /*根据方法的返回值，进行相应的客户端跳转，服务端跳转，或者仅仅是输出字符串*/
+            if(redirect.startsWith("@"))
+                response.sendRedirect(redirect.substring(1));
+            else if(redirect.startsWith("%"))
+                response.getWriter().print(redirect.substring(1));
+            else
+                request.getRequestDispatcher(redirect).forward(request,response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected  InputStream parseUpload(HttpServletRequest request, Map<String, String> params){
+        InputStream is = null;
+        try{
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            //设置上传文件的大小为10M
+            factory.setSizeThreshold(1024*10240);
+            List items = upload.parseRequest(request);
+            Iterator iter = items.iterator();
+            while (iter.hasNext()){
+                FileItem item = (FileItem) iter.next();
+                if(!item.isFormField()){
+                    is = item.getInputStream();
+                }else {
+                    String paramName = item.getFieldName();
+                    String paramValue = item.getString();
+                    paramValue = new String(paramValue.getBytes("ISO-8859-1"));
+                    params.put(paramName,paramValue);
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return is;
+    }
 }
