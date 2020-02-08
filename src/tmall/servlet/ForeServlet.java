afd6874a83a87c5fd6a43b22f1a1b949656cfd36
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-02-08 23:19:56 +0800

    commit new file:   src/tmall/servlet/ForeServlet.java

--- src/tmall/servlet/ForeServlet.java
+++ src/tmall/servlet/ForeServlet.java
@@ -1,4 +1,20 @@
 package tmall.servlet;
 
-public class ForeServlet {
+import tmall.bean.Category;
+import tmall.dao.CategoryDAO;
+import tmall.dao.ProductDAO;
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class ForeServlet extends BaseForeServlet {
+    public String home(HttpServletRequest request, HttpServletResponse response, Page page){
+        List<Category> cs= new CategoryDAO().list();
+        new ProductDAO().fill(cs);
+        new ProductDAO().fillByRow(cs);
+        request.setAttribute("cs", cs);
+        return "home.jsp";
+    }
 }
