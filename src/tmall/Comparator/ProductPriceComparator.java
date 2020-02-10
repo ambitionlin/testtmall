858f99dc6a72382c680c3c237ca4c3a66746954f
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-02-10 23:24:11 +0800

    commit src/tmall/Comparator/ProductPriceComparator.java

--- src/tmall/Comparator/ProductPriceComparator.java
+++ src/tmall/Comparator/ProductPriceComparator.java
@@ -1,4 +1,12 @@
 package tmall.Comparator;
 
-public class ProductPriceComparator {
+import tmall.bean.Product;
+
+import java.util.Comparator;
+
+public class ProductPriceComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return (int)(p1.getPromotePrice()-p2.getPromotePrice());
+    }
 }
