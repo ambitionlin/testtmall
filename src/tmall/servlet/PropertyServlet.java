2941cc43d0e86870bce5d4dbf4a057e8a81e2bc3
Wan Junlin <33538213+ambitionlin@users.noreply.github.com>
2020-02-11 23:03:10 +0800

    commit src/tmall/servlet/PropertyServlet.java

--- src/tmall/servlet/PropertyServlet.java
+++ src/tmall/servlet/PropertyServlet.java
@@ -52,15 +52,23 @@ public class PropertyServlet extends BaseBackServlet{
     }
 
     public String list(HttpServletRequest request,HttpServletResponse response,Page page){
-        int cid = Integer.parseInt(request.getParameter("cid"));
+        int cid = Integer.parseInt(request.getParameter("cid"));//获取分类cid
         Category c = categoryDAO.get(cid);
+        //基于cid,获取当前分类下的属性集合
         List<Property> ps = propertyDAO.list(cid,page.getStart(),page.getCount());
+        //获取当前分类下的属性总数，并且设置给分页page对象
         int total = propertyDAO.getTotal(cid);
         page.setTotal(total);
+        //拼接字符串"&cid="+c.getId(),设置给page对象的param值。因为属性分页都是基于当前分类下的分页，所以分页的时候需要传递这个cid
         page.setParam("&cid="+c.getId());
+        //把属性集合设置到request的"ps"属性上
         request.setAttribute("ps",ps);
+        //把分类对象设置到request的"c"属性上
         request.setAttribute("c",c);
+        //把分页对象设置到request的"page"对象上
         request.setAttribute("page",page);
+        //服务端跳转到“admin/listProperty.jsp”
         return "admin/listProperty.jsp";
+        //在listProperty.jsp页面上使用c:forEach 遍历ps集合，并显示
     }
 }
